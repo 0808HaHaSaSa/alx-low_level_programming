05026f9466ad4900fce50674725b5f26f6ffea48
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-14 19:37:25 +0100

    0-preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char i;
 long li;
 long lli;
 float f;
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of a int: %ld byte(s)\n", sizeof(i));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of a int: %d byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
