08a3df45757685901c332c0d9c8fc7ae05589825
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-14 13:36:22 +0100

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.*/
 int main(void)
 {
 /* Notice that final new line character is implicit */
