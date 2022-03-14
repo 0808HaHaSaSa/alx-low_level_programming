73970b2068563541643620e0912ace0c0ec7ba8f
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-15 00:05:31 +0100

    0-preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-fprintf(stdr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", f);
 return (1);
 }
 
