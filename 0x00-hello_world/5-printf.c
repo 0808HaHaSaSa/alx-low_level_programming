fbf1e0966ad9c81542bd93afe115bf64222b4755
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-14 16:15:03 +0100

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-*main - prints all the numers of base 16 in lowercase
-
-*return: Always 0 (success)
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 /* Notice that final new line character is implicit */
