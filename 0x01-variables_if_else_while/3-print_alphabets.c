c2849d83581dc7b23df639556ec775342f198803
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-15 03:28:06 +0100

    0-preprocessor

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,13 @@
  *
  * Return: Always 0 (Success)
 */
+/* betty style doc for function main goes there */
 int main(void)
 {
 char c;
-for(c='a'; c<='z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 putchar(c);
-for(c='A'; c<='Z'; c++)
+for (c = 'A'; c <= 'Z'; c++)
 putchar(c);
 putchar('\n');
 return (0);
