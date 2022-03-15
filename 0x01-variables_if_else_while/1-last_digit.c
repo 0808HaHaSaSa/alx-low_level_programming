1c6f623e12b4625feb692ca1269d0120a80e716f
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-15 02:14:59 +0100

    0-preprocessor

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ lastDigit = n % 10;
 printf("Last digit of %d is %d and is", n, lastDigit);
 if (lastDigit > 5)
 printf("greater than 5\n");
-else if (Last digit == 0)
+else if (lastDigit == 0)
 printf("0\n");
 else
 printf("less then 6 and not 0\n");
