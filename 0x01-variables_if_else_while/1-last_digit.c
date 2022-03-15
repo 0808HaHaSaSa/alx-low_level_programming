b47b5c53168a93e72a07d4bf0bf6e2fec3e10933
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-15 02:09:17 +0100

    0-preprocessor

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,14 +11,17 @@
 int main(void)
 {
 int n;
+int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
+lastDigit = n % 10;
+printf("Last digit of %d is %d and is", n, lastDigit);
+if (lastDigit > 5)
+printf("greater than 5\n");
+else if (Last digit == 0)
+printf("0\n");
+else
+printf("less then 6 and not 0\n");
 return (0);
 }
+
