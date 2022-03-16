eb577f4fe84be7856ba118710aa1ea9a16f903fb
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-16 01:30:03 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
- */
-/*print the alphabet*/
+ *
+ *print the alphabet
+*/
 
 void print_alphabet(void)
 {
