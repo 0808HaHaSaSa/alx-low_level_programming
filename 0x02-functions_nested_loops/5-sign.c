8b7848fde7c1ad97cda8f8c7e001f98894d9f41b
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-16 19:47:52 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,21 @@
 int print_sign(int n)
 {
 if (n == 0)
+{
 printf("0");
 return (0);
-else if (n > 0)
+}
+else
+{
+if (n > 0)
+{
 printf("+");
 return (1);
-else if (n < 0)
+}
+else
+{
 printf("-");
 return (-1);
+}
+}
 }
