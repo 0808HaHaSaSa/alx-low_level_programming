ff080d1833f96ccc5833e825a0c8a71414a75623
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-16 01:18:33 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 char A;
@@ -7,14 +12,4 @@ for (A = 'a'; A <= 'z'; A++)
 printf ("%c", A);
 printf("\n");
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 
-int main(void)
-{
-print_alphabet();
-return (0);
-}
