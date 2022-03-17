965ef43710362ae67a1adbf575be5ea080705665
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-18 00:27:24 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*print_numbers - checks for a digit
+*
+*description:checks for a digit (0 through 9).
+*Return:  always O
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 9; i++)
+printf("%d", i);
+printf("9\n");
+}
