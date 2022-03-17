3adefc326f2a501fb3891d55e14d4c3b88c1a038
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-17 01:16:28 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,20 @@
 void print_to_98(int n)
 {
 int i;
+if (n < 98)
+{
 for (i = n; i < 98; i++)
+{
 printf("%d,\t", i);
+}
 printf("98\n");
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d,\t", i);
+}
+printf("98\n");
+}
 }
