e4df25cfd2c1ee000bd1a8479eb4da2f147f8fe8
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-17 01:06:22 +0100

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * test_islower - Teste la fonction _islower
+ *
+ * @n : nombre à transmettre à la fonction _islower
+ */
+void print_to_98(int n)
+{
+int i;
+for (i = n; i < 98; i++)
+printf("%d,\t", i);
+printf("98\n");
+}
