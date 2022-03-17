fec6e479bc5f82cbe9323df265ef6f75d056e14b
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-17 23:56:04 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*mul - do multiplies two integers
+*@a: it is the argurments
+*@b: it is the argument
+*description:do multiplies two integers
+*Return:  return a*b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
