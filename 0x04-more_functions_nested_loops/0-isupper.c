376b3d4c97aeb8ca706160480001388734ca9086
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-17 23:29:28 +0100

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
+/**
+*_isupper - checks for uppercase character
+*@c : it is the argurment
+*description:checks for uppercase character
+*Return:  1 if character is uppercase else return 0
+*/
 int _isupper(int c)
 {
 int i;
-i=isupper(c);
+i = isupper(c);
 if (i == 0)
 return (0);
-else 
+else
 return (1);
 }
