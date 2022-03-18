2342e31b66eb25a052b18d31febaeeaa9f8416c3
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-18 20:19:09 +0100

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*print_square - draws a straight line in the terminal
+*@size:it the argument
+*description:draws a straight line in the terminal
+*Return:  always 0
+*/
+void print_square(int size)
+{
+int j,i;
+if (size > 0)
+{
+for (j = 0; j < size; j++)
+{
+for (i = 0; i < size; i++)
+{
+printf("#");
+}
+printf("\n");
+}
+}
+else
+{
+printf("\n");
+}
+}
