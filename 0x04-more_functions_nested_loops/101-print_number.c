519bcad08d838db846eb18824372fccf6333e6d5
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-18 22:46:47 +0100

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*print_number - draws a straight line in the terminal
+*@n:it the argument
+*description:draws a straight line in the terminal
+*Return:  always 0
+*/
+void print_number(int n)
+{
+printf("%d", n);
+}
