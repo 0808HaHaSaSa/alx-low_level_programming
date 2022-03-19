74b51d9de29d85bdfa6568dda8e819a0f15098ef
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-19 01:22:01 +0100

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 
 void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
-void positive_or_negative(int i);
+void 0-positive_or_negative(int i);
 
 #endif /* MAIN_H */
