4649b423098a9f3d645bbbd928b238634b6a4163
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-22 00:18:53 +0100

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*puts_half- prints half of a string
+*@str:it the argument
+*description:prints half of a string
+*Return:  always 0
+*/
+void puts_half(char *str)
+{
+int n, i;
+n = strlen(str);
+if (n + 1 % 2 == 0)
+for (i = n + 1 / 2 ; i < n ; i++)
+putchar(str[i]);
+else
+for (i = n / 2 ; i < n ; i++)
+putchar(str[i]);
+printf("\n");
+
+}
