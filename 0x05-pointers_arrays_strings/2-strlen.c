e2168c2e84567c0603e68ef9ba1ed11b57415774
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-21 20:08:49 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_strlen - the length of a string.
+*@s:it the argument
+*description:the length of a string.
+*Return:  always the length
+*/
+int _strlen(char *s)
+{
+int n;
+n = strlen(s);
+return (n);
+}
