2b564e058d865831e123098518bbbe2afb0696e7
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-21 20:23:29 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_puts - prints a string, followed by a new line
+*@str:it the argument
+*description:prints a string, followed by a new line
+*Return:  always 0
+*/
+void _puts(char *str)
+{
+puts(str);
+}
