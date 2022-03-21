3b7a8d3392ea9c51d8de441a821808444f8f3adb
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-21 19:37:23 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*reset_to_98 - draws a straight line in the terminal
+*@n:it the argument
+*description:draws a straight line in the terminal
+*Return:  always 0
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
