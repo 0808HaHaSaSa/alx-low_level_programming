c4f07abcf9f1e6ffedaeefa2734ec2d3fd2b9a56
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-23 23:02:13 +0100

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**_strcmp - compares two strings.
+*@s2:it the argument
+*@s1:it the argument
+*description: compares two strings.
+*Return:  always a string
+*/
+int _strcmp(char *s1, char *s2)
+{
+int *n;
+n = strcmp(s1, s2);
+return (n);
+
+}
