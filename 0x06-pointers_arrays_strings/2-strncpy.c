270de618143352ca0904967fe0558939f4f78c86
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-23 22:37:54 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**_strcat - copies a string.
+*@dest:it the argument
+*@src:it the argument
+* @n:it the argument
+*description: copies a string.
+*Return:  always a string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+char *m;
+m = strncpy(dest, src, n);
+return (m);
+
+}
