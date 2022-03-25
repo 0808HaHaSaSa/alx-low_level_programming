25b4fb61118c057bd26ca571bde5c4452ab6ecac
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-25 01:19:38 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+/**
+***cap_string - capitalizes all words of a string.
+*@str:it the argument
+*description: capitalizes all words of a string.
+*Return:  always 0
+*/
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '\t')
+{
+str[i] = str[i] - 32;
+}
+else if (str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?')
+{
+str[i] = str[i] - 32;
+}
+else if (str[i - 1] == ';' || str[i - 1] == ',' || str[i - 1] == '"')
+{
+str[i] = str[i] - 32;
+}
+else if (str[i - 1] == '{' || str[i - 1] == '}')
+{
+str[i] = str[i] - 32;
+}
+else if (str[i - 1] == '(' || str[i - 1] == ')')
+{
+str[i] = str[i] - 32;
+}
+}
+}
+return (str);
+}
