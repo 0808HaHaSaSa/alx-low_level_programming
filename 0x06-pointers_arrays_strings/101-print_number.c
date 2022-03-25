de1e89c6a953d7c0e2301a672d5e935fce326810
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-25 01:50:24 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**print_number - capitalizes all words of a string.
+*@n:it the argument
+*description: capitalizes all words of a string.
+*Return:  always 0
+*/
+void print_number(int n)
+{
+int i;
+printf("%d", n);
+}
