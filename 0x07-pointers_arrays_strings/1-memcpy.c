11629b903e5cc0b19ce94ea5f5128a3bf74475da
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-28 18:10:59 +0200

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+***_memcpy - copies memory area.
+*@n:it the argument
+*@dest:it argument
+*@src:argument
+*description: copies memory area.
+*Return:  always 0
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *d;
+d = memcpy(dest, src, n);
+return (d);
+}
