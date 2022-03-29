1129b73750f72b77727a8c2166b0029e143b23d3
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-29 02:13:16 +0200

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**print_chessboard - locates a substring.
+*@a:it the argument
+*
+*description:locates a substring.
+*Return:  always 0
+*/
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
