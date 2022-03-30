acbef2c4921d684125a0723ea0058f3ce4e0af38
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-31 00:18:06 +0200

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**_puts_recursion - prints a string, followed by a new line.
+*@s:it the argument
+*description: prints a string, followed by a new line.
+*Return:  always 0
+*/
+void _puts_recursion(char *s)
+{
+puts(s);
+}
