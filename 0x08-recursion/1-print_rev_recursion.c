4971e8a3e47152744dbd66f2143483a77e30f2b4
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-31 00:31:38 +0200

    0-puts_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**_print_rev_recursion - prints a string in reverse.
+*@s:it the argument
+*description: prints a string in reverse.
+*Return:  always 0
+*/
+void _print_rev_recursion(char *s)
+{
+int n, i;
+n = strlen(s);
+for (i = n - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+}
