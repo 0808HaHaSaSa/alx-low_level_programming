a7ca44cb20db7293bbcd7b1964323020e783a025
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-31 01:41:15 +0200

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+**_pow_recursion -   the value of x raised to the power of y.
+*@y:it the argument
+*@x:it the argument
+*description:  the value of x raised to the power of y.
+*Return:  always the raised to the power
+*/
+int _pow_recursion(int x, int y)
+{
+int i;
+if (y < 0)
+{
+return (-1);
+}
+else
+{
+i = pow(x, y);
+return (i);
+}
+}
