78b603640f2a61dda22d1482e107aa600d07f7e7
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-30 04:43:06 +0200

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**set_string - compares two strings.
+*@s:it the argument
+*@to:it the argument
+*description: compares two strings.
+*Return:  always 0
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
