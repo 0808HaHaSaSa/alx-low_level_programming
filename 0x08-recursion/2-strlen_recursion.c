784b95ca16fcb50ae9b7a58b9cb309bac745b78a
root <root@DESKTOP-VB97GVI.localdomain>
2022-03-31 00:45:40 +0200

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+**_strlen_recursion - returns the length of a string
+*@s:it the argument
+*description: returns the length of a string
+*Return:  always the length
+*/
+int _strlen_recursion(char *s)
+{
+int n;
+n = strlen(s);
+return (n);
+}
