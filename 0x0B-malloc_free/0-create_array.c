064880dc9e0869536f9df774c60357545776ccda
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-07 00:34:39 +0200

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+***create_array - creates an array of chars
+*@size:it the argument
+*@c:it the argument
+*description: initializes it with a specific char.
+*Return:  always 0
+*/
+char *create_array(unsigned int size, char c)
+{
+char *tab = NULL;
+tab = malloc(sizeof(char) * size);
+int i;
+for (i = 0; i < size; i++)
+{
+tab[i] = c;
+}
+return (tab);
+}
