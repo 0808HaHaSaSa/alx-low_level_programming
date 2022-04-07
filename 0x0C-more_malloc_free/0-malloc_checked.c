fbc22ceb880a866a314383536b81f3c54986135e
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-07 23:32:46 +0200

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+****malloc_checked - creates an array of chars
+*@b:it the argument
+*description: initializes it with a specific char.
+*Return:  always 0
+*/
+void *malloc_checked(unsigned int b)
+{
+int *tab = NULL;
+tab = malloc(b);
+if (tab == NULL)
+exit(98);
+return (tab);
+}
