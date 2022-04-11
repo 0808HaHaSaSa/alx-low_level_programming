b5169ff6aa7b69f04779e1a1b8636099ac3bd435
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-12 00:03:16 +0200

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#ifdef DOG_H
+#define DOG_H
+struct dog{
+char *name;
+float age;
+char *owner;
+/* declare as many members as desired, but the entire structure size must be known to the compiler. */
+};
+#endif
