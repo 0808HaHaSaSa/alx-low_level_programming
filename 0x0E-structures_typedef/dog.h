0ff1dcb4434735a387a0e65a08d034665e37ed4b
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-13 00:48:16 +0200

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,4 +6,5 @@ float age;
 char *owner;
 /* declare as many members as desired, but the entire structure size must be known to the compiler. */
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
