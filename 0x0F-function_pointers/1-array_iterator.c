b5f7f638ef92e01b43a892e97c1e6e501ba65448
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-14 03:30:18 +0200

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+**array_iterator - executes a function .
+*@size:it the argument
+*@action:it the argument
+*@array:argument
+*description:function given as a parameter on each element of an array.
+*Return:  always 0
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
