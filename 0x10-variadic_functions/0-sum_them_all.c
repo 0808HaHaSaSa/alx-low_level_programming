199ce6d6f678dd337bae8d8df1c9809f857e3f7b
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-15 02:58:30 +0200

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
-***string_nconcat - the sum of all its parameters.
+**sum_them_all - the sum of all its parameters.
 *@n:it the argument
 *description: the sum of all its parameters.
 *Return:  always 0
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 int sum = 0, i;
 va_list ap;
 va_start(ap, n);
-for (i = 0; i < n;i++)
+for (i = 0; i < n; i++)
 sum += va_arg(ap, int);
 va_end(ap);
 return (sum);
