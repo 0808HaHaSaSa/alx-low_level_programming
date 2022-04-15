8095410a20e9900ccb6a11a248760ccad583f88e
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-15 03:37:26 +0200

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+***print_numbers - the sum of all its parameters.
+*@n:it the argument
+*@separator:argument
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+int sum=0, i;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+printf("%d%s ",va_arg(ap,int),separator);
+va_end(ap);
+putchar("\n");
+}
