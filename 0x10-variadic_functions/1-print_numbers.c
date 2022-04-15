ad3824f64cd9504024d13802a7457abd2f2812a3
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-15 14:27:22 +0200

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,8 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list ap;
 va_start(ap, n);
-for (i = 0; i < n; i++)
+for (i = 0; i < n - 1; i++)
 printf("%d%s ", va_arg(ap, int), separator);
+printf("%d", va_arg(ap, int));
 va_end(ap);
 printf("\n");
 }
