5ab436e72ae039906ad35ca1ad280d76b78d21c2
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-15 02:54:36 +0200

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+***string_nconcat - the sum of all its parameters.
+*@n:it the argument
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+int sum = 0, i;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n;i++)
+sum += va_arg(ap, int);
+va_end(ap);
+return (sum);
+}
