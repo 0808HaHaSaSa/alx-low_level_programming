b4b69ec5e243bba8c79e6743d3b3d28cd4155a27
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-18 14:48:05 +0200

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 int i;
-for (i = 48; i < 57; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
+if(i != 57)
 putchar(',');
 putchar(32);
 }
-putchar(57);
+putchar('\n');
 return (0);
 }
-
