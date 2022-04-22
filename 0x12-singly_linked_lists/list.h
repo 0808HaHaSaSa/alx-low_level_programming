de7914c5f008bb938b7caf98b37716b0f4049643
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-22 03:18:38 +0200

    first

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef __LIST__H__
-#define __LIST__H__
+#ifndef LIST_H
+#define LIST_H
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
