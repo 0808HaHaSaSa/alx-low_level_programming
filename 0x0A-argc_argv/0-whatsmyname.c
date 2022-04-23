87b9cb21a984c3851ea5d2719f4504cd616c6cb2
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-24 00:14:43 +0200

    correction

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main (int argc, char *argv[])
+{
+printf("%d",argc);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *@argc:argument
+ *@argv:argumeent
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
