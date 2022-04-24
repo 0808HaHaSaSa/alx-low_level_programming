06b0032875f068d14c2af34a256d1a001999025c
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-24 16:03:54 +0200

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
@@ -11,6 +10,7 @@
 /* betty style doc for function main goes there */
 int main(int argc, char *argv[])
 {
+int m;
 if (argc != 3)
 {
 printf("Error\n");
@@ -18,7 +18,8 @@ return (1);
 }
 else
 {
-printf("%d", (int)((int)argv[1]*(int)argv[2]));
+m=(int)((*argv[1])*(*argv[2]));
+printf("%d", m);
 printf("\n");
 return (0);
 }
