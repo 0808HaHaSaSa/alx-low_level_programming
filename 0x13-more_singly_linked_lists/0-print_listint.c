c6b194935cc8c3bd9ac9a83c95ec42a52dd1b57d
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-25 19:38:58 +0200

    correction

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+**print_listint - produit une sortie selon un format.
+*@h:it the argument
+*description: produces output according to a format.
+*Return:  always 0
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t size = 0;
+while (h != NULL)
+{
+size++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (size);
+}
