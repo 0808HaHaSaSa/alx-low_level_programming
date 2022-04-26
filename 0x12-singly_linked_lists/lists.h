b2ae186b7d47ef8a28962acd95db03b28cedb449
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-26 07:19:02 +0200

    correction

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,11 +2,11 @@
 #define __LISTS__H__
 #include <stdio.h>
 #include <stdlib.h>
-size_t print_list(const list_t *h);
+size_t print_list(const sigset_t *h);
 size_t list_len(const sigset_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+list_t *add_node(sigset_t **head, const char *str);
+list_t *add_node_end(sigset_t **head, const char *str);
+void free_list(sigset_t *head);
 typedef struct list_s
 {
     char *str;
