964f3a76cbff8888b0d5ec175af672fe6bc08f8b
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-26 07:25:08 +0200

    correction

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,7 +8,7 @@
 *description: produces output according to a format.
 *Return:  always 0
 */
-size_t list_len(const list_t *h)
+size_t list_len(const sigset_t *h)
 {
 size_t size = 0;
 if (h != NULL)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 size_t print_list(const sigset_t *h);
 size_t list_len(const sigset_t *h);
-list_t *add_node(sigset_t **head, const char *str);
-list_t *add_node_end(sigset_t **head, const char *str);
+sigset_t *add_node(sigset_t **head, const char *str);
+sigset_t *add_node_end(sigset_t **head, const char *str);
 void free_list(sigset_t *head);
 typedef struct list_s
 {
