996d1ea6f64333b89cbb3665cb8bd13607196da1
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-27 15:06:42 +0200

    correction

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+**listint_len - produit une sortie selon un format.
+*@h:it the argument
+*description: produces output according to a format.
+*Return:  always 0
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t size = 0;
+while (h != NULL)
+{
+h = h->next;
+size++;
+}
+return (size);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,5 +14,5 @@ int n;
 struct listint_s *next;
 } listint_t,*List;
 size_t print_listint(const listint_t *h);
-
+size_t listint_len(const listint_t *h);
 #endif
