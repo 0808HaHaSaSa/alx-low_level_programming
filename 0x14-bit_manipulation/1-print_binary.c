4e7ffccb7e654ff66542805b140a60d2731772b3
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-29 12:38:05 +0200

    correction

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+**print_binary - the sum of all its parameters.
+*@n:it the argument
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+{
+print_binary(n / 2);
+}
+printf("%d", n % 2);
+}
