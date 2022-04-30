b80fd71d7d5d6434482aca86fd8307a057d4cdca
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-30 19:08:24 +0200

    correction

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+**free_listint - produit une sortie selon un format.
+*@head:it the argument
+*description: produces output according to a format.
+*Return:  always 0
+*/
+void free_listint(listint_t *head)
+{
+    listint_t *h,*c;
+c=h;
+while(c!=NULL)
+{
+    h=c->next;
+    free(c);
+    c=h;
+}
+(head)=NULL;
+}
