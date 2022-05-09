ee26a6342ee23daf8f384938161c96d72d014532
root <root@DESKTOP-VB97GVI.localdomain>
2022-05-09 04:19:35 +0200

    correction

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+**print_times_table - the sum of all its parameters.
+*@n:argument
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+void print_times_table(int n)
+{
+int i, j;
+if ((n < 15) && (n > 0))
+{
+for (i = 0; i < n + 1; i++)
+{
+for (j = 0; j < n + 1; j++)
+{
+printf("%d", i * j);
+if (j != n)
+{
+if ((i * (j + 1)) <= 9)
+{
+printf(",   ");
+}
+else
+{
+if ((i * (j + 1)) <= 99)
+{
+printf(",  ");
+}
+else
+{
+printf(", ");
+}
+}
+}
+}
+printf("\n");
+}
+}
+}
