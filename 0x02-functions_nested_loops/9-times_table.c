ad1f0a872f5eeef15a1829654033f366e9d0f42e
root <root@DESKTOP-VB97GVI.localdomain>
2022-05-09 03:33:22 +0200

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,18 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-printf("%d", i*j);
+printf("%d", i * j);
 if (j != 9)
+{
+if (i * j <= 9)
+{
+printf(",  ");
+}
+else
+{
 printf(", ");
+}
+}
 }
 printf("\n");
 }
