79e11081c39564e993925fc0fdb579e7782c3c53
root <root@DESKTOP-VB97GVI.localdomain>
2022-05-09 03:05:50 +0200

    correction

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+**times_table - the sum of all its parameters.
+*
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+void times_table(void)
+{int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j=0;j<10;j++)
+{
+printf("%d", i*j);
+if (j != 9)
+printf(", ");
+}
+printf("\n");
+}
+}
