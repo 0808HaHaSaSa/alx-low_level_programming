c8d381238d4c0298a0362062f6bdfc1c6f49b88c
root <root@DESKTOP-VB97GVI.localdomain>
2022-05-10 15:30:10 +0200

    correction

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+**fibonacci - the sum of all its parameters.
+*
+*description: the sum of all its parameters.
+*Return:  always 0
+*/
+void fibonacci(void)
+{
+int i = 0, j = 1, s = 1;
+while (s < 50)
+{
+printf("%d, ", s);
+s = j + i;
+i = j;
+j = s;
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,5 @@ void times_table(void);
 void print_times_table(int n);
 void natural(void);
 void jack_bauer(void);
+void fibonacci(void);
 #endif
