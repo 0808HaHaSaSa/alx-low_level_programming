9af08dc114aa39b18615546bdc3db4fe32c87932
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-16 12:06:36 +0100

    two

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <string.h>
+#include "lists.h"
+
 /**
-**add_node - produces output according to a format.
-*@str:it the argument
-*description: produces output according to a format.
-*Return:  always 0
-*/
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to pointer of first node of list_t list
+ * @str: string to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *ele;
-ele = malloc(sizeof(*ele));
-if(ele == NULL)
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(const char *s)
 {
-fprintf(stderr,"errer\n");
-exit (EXIT_FAILURE);
-}
-ele->str = *str;
-ele->
-if(head == NULL)
-{
-ele->next = NULL;
-}
-else{
-ele->next = *head;
-}
-return (ele);
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
 }
