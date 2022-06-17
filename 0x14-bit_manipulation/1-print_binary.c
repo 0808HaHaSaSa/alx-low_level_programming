44a85f4e859ac1acff9cb3293ce6f7110c0008b9
root <root@DESKTOP-VB97GVI.localdomain>
2022-06-17 02:42:49 +0100

    choix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,34 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include "holberton.h"
+
 /**
-**print_binary - the sum of all its parameters.
-*@n:it the argument
-*description: the sum of all its parameters.
-*Return:  always 0
-*/
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed in binary
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-for (int i = sizeof(int) * 4; i >= 0; i--)
-printf("%d", (n & (1 << i)) >> i );
+	int binary;
+	int zero = 0; /* flag to make sure leading 0s are not printed */
+	int size; /* size in bits of n, minus 1 for +/- bit */
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	size = sizeof(n) * 8 - 1;
+	for (i = size; i >= 0; i--)
+	{
+		binary = n >> i;
+		if (binary & 1)
+		{
+			_putchar('1');
+			zero = 1;
+		}
+		else if (zero == 1)
+			_putchar('0');
+	}
 }
