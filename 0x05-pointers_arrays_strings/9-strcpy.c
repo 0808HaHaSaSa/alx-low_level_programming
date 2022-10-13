4c06e5d872dd873a9418b130886ecfe6e36cc305
root <root@DESKTOP-VB97GVI.localdomain>
2022-10-13 12:23:09 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_strcpy - prints andd copies the string
+*@dest:it the argument
+*@src:it the argument
+*description: prints n elements of an array of integers
+*Return:  always 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return dest;
+}
