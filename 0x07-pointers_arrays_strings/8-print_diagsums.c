70284b0143a147dd7db84d3f0adcd535054ed6ee
root <root@DESKTOP-VB97GVI.localdomain>
2022-10-19 14:01:01 +0100

    ok

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,17 +11,17 @@ void print_diagsums(int *a, int size)
 {
 int i, b, m;
 unsigned int j = 0, k = 0;
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-for(b = 0; b < size; b++)
+for (b = 0; b < size; b++)
 {
-if(i == b)
+if (i == b)
 j = j + (*((int *) a + size * b + i));
 }
 }
-for(m = 0; m < size; m++)
+for (m = 0; m < size; m++)
 {
-k = k + (*((int *) a + m *size+(size - 1 - m)));
+k = k + (*((int *) a + m * size + (size - 1 - m)));
 }
 printf("%d, %d\n", j, k);
 }
