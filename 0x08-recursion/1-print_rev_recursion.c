53fd6d6d6b606b835a0d21ea0857c568fabd5272
root <root@DESKTOP-VB97GVI.localdomain>
2022-10-20 07:30:42 +0100

    ok

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
-**_print_rev_recursion - prints a string in reverse.
-*@s:it the argument
-*description: prints a string in reverse.
-*Return:  always 0
-*/
+ * print_rev_recursion - function that prints a string in reverse.
+ * @s: array name
+ * Return: void
+ */
 void _print_rev_recursion(char *s)
 {
-int n, i;
-n = strlen(s);
-for (i = n - 1; i >= 0; i--)
-{
-printf("%c", s[i]);
-}
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
