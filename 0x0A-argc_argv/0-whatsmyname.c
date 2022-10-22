7977e09cfa3b441290ee4559ec81b2cc0f98ac91
root <root@DESKTOP-VB97GVI.localdomain>
2022-10-22 15:10:55 +0100

    ok

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,13 @@
 /* more headers goes there */
 /**
  * main - Entry point
- *
+ *@argc:argument
+ *@argv:argument
  * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
 int main(int argc, char *argv[])
 {
-printf("%s\n",argv[argc - 1]);
+printf("%s\n", argv[argc - 1]);
 return (0);
 }
