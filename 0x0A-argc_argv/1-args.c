fa117d560e3c205b22655aa1d5a77f4f4bb91e7a
root <root@DESKTOP-VB97GVI.localdomain>
2022-10-24 00:07:37 +0100

    ok

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 printf("%d\n", argc);
 return (0);
